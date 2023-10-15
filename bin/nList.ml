662d1fcb2474dc4a8a1922231da18abe9f71a4dd
triplewhopper <34619815+triplewhopper@users.noreply.github.com>
2023-10-16 03:01:22 +0900

    modified pretty printing for token and nlist

--- bin/nList.ml
+++ bin/nList.ml
@@ -1,4 +1,23 @@
-type 'a t = Nested of 'a t list | Singleton of 'a [@@deriving show]
+type 'a t = Nested of 'a t list | Singleton of 'a
+
+let pp pp_elt ppf =
+  let empty_flag = ref true in
+  let rec pp' pp_elt ppf = function
+    | Nested [] -> ()
+    | Nested (Singleton x :: xs) ->
+        if !empty_flag = false then Format.fprintf ppf ";@ ";
+        Format.fprintf ppf "%a" pp_elt x;
+        empty_flag := false;
+        pp' pp_elt ppf (Nested xs)
+    | Nested (Nested x :: xs) ->
+        Format.fprintf ppf "%a" (pp' pp_elt) (Nested x);
+        pp' pp_elt ppf (Nested xs)
+    | Singleton x ->
+        if !empty_flag = false then Format.fprintf ppf ";@ ";
+        Format.fprintf ppf "%a" pp_elt x;
+        empty_flag := false
+  in
+  Format.fprintf ppf "@[<hov 2>[%a]@]" (pp' pp_elt)
 
 let rec is_empty = function
   | Nested [] -> true
@@ -15,11 +34,11 @@ let rec front = function
   | Nested (Nested x :: xs) -> (
       match front (Nested x) with None -> front (Nested xs) | Some x -> Some x)
   | Singleton x -> Some x
+
 let rec back = function
   | Nested [] -> None
   | Nested (Singleton x :: []) -> Some x
   | Nested (Singleton _ :: xs) -> back (Nested xs)
   | Nested (Nested x :: xs) -> (
-      match back (Nested xs) with None -> back (Nested x) | Some x -> Some x
-  )
+      match back (Nested xs) with None -> back (Nested x) | Some x -> Some x)
   | Singleton x -> Some x
--- bin/token.ml
+++ bin/token.ml
@@ -3,8 +3,23 @@ type position = Lexing.position = {
   pos_lnum : int;
   pos_bol : int;
   pos_cnum : int;
-} [@@deriving show]
-type t = {start: position; stop: position; text: string} [@@deriving show]
+}
+[@@deriving show]
 
-let make (start, stop) text = {start; stop; text}
+type t = { start : position; stop : position; text : string }
 
+let make (start, stop) text = { start; stop; text }
+
+let pp fmt { start; stop; text } =
+  if start.pos_fname = stop.pos_fname then
+    Format.fprintf fmt "@[%s:%d:%d-%d:@ \"%s\"@]" start.pos_fname start.pos_lnum
+      (start.pos_cnum - start.pos_bol)
+      (stop.pos_cnum - stop.pos_bol)
+      (String.escaped text)
+  else
+    Format.fprintf fmt "@[%s:%d:%d-%d:%d:@ \"%s\"@]" start.pos_fname
+      start.pos_lnum
+      (start.pos_cnum - start.pos_bol)
+      stop.pos_lnum
+      (stop.pos_cnum - stop.pos_bol)
+      (String.escaped text)
