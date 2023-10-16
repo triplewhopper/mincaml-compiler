4389c63e3573d779898cb6f9c512815760da6881
triplewhopper <34619815+triplewhopper@users.noreply.github.com>
2023-10-16 15:19:53 +0900

    changed pp function for Token.t, to display like '<text>@<loc>'

--- bin/token.ml
+++ bin/token.ml
@@ -12,14 +12,13 @@ let make (start, stop) text = { start; stop; text }
 
 let pp fmt { start; stop; text } =
   if start.pos_fname = stop.pos_fname then
-    Format.fprintf fmt "@[%s:%d:%d-%d:@ \"%s\"@]" start.pos_fname start.pos_lnum
+    Format.fprintf fmt "@[<h 0>\"%s\"%@%s:%d:%d-%d@]" (String.escaped text)
+      start.pos_fname start.pos_lnum
       (start.pos_cnum - start.pos_bol)
       (stop.pos_cnum - stop.pos_bol)
-      (String.escaped text)
   else
-    Format.fprintf fmt "@[%s:%d:%d-%d:%d:@ \"%s\"@]" start.pos_fname
-      start.pos_lnum
+    Format.fprintf fmt "@[<h 0>\"%s\"%@%s:%d:%d-%d:%d@]" (String.escaped text)
+      start.pos_fname start.pos_lnum
       (start.pos_cnum - start.pos_bol)
       stop.pos_lnum
       (stop.pos_cnum - stop.pos_bol)
-      (String.escaped text)
